ea68d75186b09e7ad011c756150b27c48d18f58e
unknown <aliakber@requity.ca>
2022-05-12 15:21:29 -0400

    Started Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace program // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prss any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
