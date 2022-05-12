cde30c94861376045940c5940ca25f937e3631d6
unknown <aliakber@requity.ca>
2022-05-12 15:38:15 -0400

    updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace program // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prss any key to close...");
+            Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
     }
